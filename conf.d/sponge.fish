cb8f68c282e223121598f400150c55079071d6dd
dkmar <dkmar@protonmail.com>
2025-07-24 02:46:01 -0700

    maintain cleaner history + let the fzf hist widget delete entries

---
+++ conf.d/sponge.fish
@@ -0,0 +1,32 @@
+status is-interactive || exit
+
+# Sponge version
+# set -g sponge_version 1.1.0
+
+# Set global variables with defaults
+set -g sponge_delay 5
+set -g sponge_purge_only_on_exit true
+set -g sponge_filters sponge_filter_failed # sponge_filter_matched
+set -g sponge_allow_previously_successful false
+set -g sponge_successful_exit_codes 0
+set -g sponge_regex_patterns
+
+# Verify event handlers are available
+functions --query \
+    _sponge_on_prompt \
+    _sponge_on_preexec \
+    _sponge_on_postexec \
+    _sponge_on_exit
+
+# Initialize state on install
+function _sponge_install --on-event sponge_install
+    set -g _sponge_current_command ''
+    set -g _sponge_current_command_exit_code 0
+    set -g _sponge_current_command_previously_in_history false
+end
+
+# Clean up on uninstall
+function _sponge_uninstall --on-event sponge_uninstall
+    _sponge_clear_state
+    # set --erase sponge_version
+end
--- functions/_fzf_search_history.fish
+++ functions/_fzf_search_history.fish
@@ -10,6 +10,7 @@ function _fzf_search_history --description "Search command history. Replace the 
         set -f fzf_history_time_format "%m-%d %H:%M:%S"
     end
 
+    set flag_history_edit _EDIT_HISTORY
     # Delinate time from command in history entries using the vertical box drawing char (U+2502).
     # Then, to get raw command from history entries, delete everything up to it. The ? on regex is
     # necessary to make regex non-greedy so it won't match into commands containing the char.
@@ -25,6 +26,7 @@ function _fzf_search_history --description "Search command history. Replace the 
             --query=(commandline) \
             --preview="string replace --regex '$time_prefix_regex' '' -- {} | fish_indent --ansi" \
             --preview-window="bottom:3:wrap" \
+            --bind="shift-delete:become:echo $flag_history_edit; [ \$FZF_SELECT_COUNT -eq 0 ]; and echo {}; or cat {+f}" \
             $fzf_history_opts |
         string split0 |
         # remove timestamps from commands selected
@@ -32,7 +34,13 @@ function _fzf_search_history --description "Search command history. Replace the 
     )
 
     if test $status -eq 0
-        commandline --replace -- $commands_selected
+        if test "$commands_selected[1]" = "$flag_history_edit"
+            for cmd in $commands_selected[2..]
+                history delete --exact --case-sensitive -- $cmd
+            end
+        else
+            commandline --replace -- $commands_selected
+        end
     end
 
     commandline --function repaint
